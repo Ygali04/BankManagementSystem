3a54ab7a825385000c2c3a99649534a7ea26114b
Yahvin Gali <ygali@g.ucla.edu>
2023-10-09 13:47:08 -0700

    Update BSTNode.cpp

--- BSTNode.cpp
+++ BSTNode.cpp
@@ -5,10 +5,10 @@ BSTNode::BSTNode()
 	left = nullptr;
 	right = nullptr;
 	name = "";
-    address = "";
-    account_num = 0;
-    pswd = 0;
-    balance = 0;
+	address = "";
+	account_num = 0;
+	pswd = 0;
+	balance = 0;
 }
 
 BSTNode::BSTNode(string name, string address, int accountno, int password, int balance)
