0e5380901ca7bdcf84810f5aed028bcfdc75a58c
Yahvin Gali <ygali@g.ucla.edu>
2023-10-09 14:17:36 -0700

    Update BST_Tree.h

--- BST_Tree.h
+++ BST_Tree.h
@@ -7,26 +7,26 @@
 class BST_Tree
 {
 	vector <int> v;
-    public:
-        BST_Tree();         //default constructor
-	    Hashtable h;        //hastable for account access hierarchy
-	    BSTNode *Root;      //root of account tree
+    	public:
+		BST_Tree();         //default constructor
+	    	Hashtable h;        //hastable for account access hierarchy
+	    	BSTNode *Root;      //root of account tree
 	    
-        //ACCOUNT OPS
-        void addAccount(string, string, int, int, int);
-	    BSTNode* BST_Tree::deleteAccount(BSTNode *, int);
-        void editAccountByAdmin();
+        	//ACCOUNT OPS
+        	void addAccount(string, string, int, int, int);
+	    	BSTNode* BST_Tree::deleteAccount(BSTNode *, int);
+        	void editAccountByAdmin();
 
-        //CRUDS
-	    void withdraw(int,int);
-	    void deposit(int,int);
-	    void transfer(int,int,int);
-	    void transactionHistory();
+        	//CRUDS
+	   	 void withdraw(int,int);
+	    	void deposit(int,int);
+	    	void transfer(int,int,int);
+	    	void transactionHistory();
 
-        //HELPER FUNCTIONS
-	    void findMax(BSTNode*);
-	    void loadServer();
-	    void updateServer(BSTNode *);
-	    BSTNode* search(BSTNode*,int);
-	    void printInfo(BSTNode*);
+        	//HELPER FUNCTIONS
+	    	void findMax(BSTNode*);
+	    	void loadServer();
+	    	void updateServer(BSTNode *);
+	    	BSTNode* search(BSTNode*,int);
+	    	void printInfo(BSTNode*);
 }
